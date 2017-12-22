7740817456bdf234d911ca38dbe00adeb2e89393
yue-b <yue@byuedeMacBook-Pro.local>
2017-12-22 17:24:12 +0800

    yyh代码

--- ItemLayer.cpp
+++ ItemLayer.cpp
@@ -51,5 +51,25 @@ bool ItemLayer::init()
 		origin.y + visibleSize.height*0.95));
 	this->addChild(refresh, 0);
 
+	//圆点
+	DrawNode* drawNode = DrawNode::create();
+	this->addChild(drawNode, 1);
+	drawNode->drawDot(Vec2(visibleSize.width / 2, visibleSize.height / 2 + 200), 10, Color4F::BLACK);
+	//多边形
+	Vec2 point2[4];
+	point2[0] = Vec2(80, 50);
+	point2[1] = Vec2(80, 680);
+	point2[2] = Vec2(1000, 680);
+	point2[3] = Vec2(1000, 50);
+	drawNode->drawPolygon(point2, 4, Color4F(1, 0, 0, 0), 2, Color4F(0, 1, 0, 1));//1000透明
+
+	Vec2 point1[4];
+	point1[0] = Vec2(visibleSize.width / 2 - 80, visibleSize.height / 2 + 180);
+	point1[1] = Vec2(visibleSize.width / 2 - 80, visibleSize.height / 2 + 190);
+	point1[2] = Vec2(visibleSize.width / 2 + 80, visibleSize.height / 2 + 190);
+	point1[3] = Vec2(visibleSize.width / 2 + 80, visibleSize.height / 2 + 180);
+	drawNode->drawPolygon(point1, 4, Color4F(0, 1, 0, 1), 1, Color4F(0, 1, 0, 1));
+
+
 	return true;
 }
